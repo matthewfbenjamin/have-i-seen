ab0ce703433d473ee639c2f7953166cc7a10ae1d
matthewfbenjamin <mbenjamin618@gmail.com>
2020-03-30 11:06:10 -0700

    Update text

--- src/App.js
+++ src/App.js
@@ -6,18 +6,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h3>
+          Has Matt Seen:
+        </h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Tiger King?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          NO.
+        </h1>
       </header>
     </div>
   );
